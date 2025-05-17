beafb80478c8cbd205695fcd8af20a9b0031cad8
Dimas Firmansyah <deirn@bai.lol>
2025-05-17 14:55:21 +0700

    add dart's pub-cache to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -41,6 +41,7 @@ fish_add_path $HOME/.config/emacs/bin
 fish_add_path $HOME/.local/share/JetBrains/Toolbox/bin
 fish_add_path $HOME/.local/share/JetBrains/Toolbox/scripts
 fish_add_path $HOME/Android/Sdk/platform-tools
+fish_add_path $HOME/.pub-cache/bin
 fish_add_path $FEDORACFG/bin
 # [ path variables ]
 
